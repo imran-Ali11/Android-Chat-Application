b329fee0b2c1683d987a656ca44c8cca735685cd
imran-Ali11 <imran.ali313924@gmail.com>
2024-08-02 21:34:15 -0700

    Android chat app

---
+++ app/src/main/java/com/example/chatapplication/listeners/ConversionListener.java
@@ -0,0 +1,8 @@
+package com.example.chatapplication.listeners;
+
+import com.example.chatapplication.models.User;
+
+public interface ConversionListener {
+
+    void onConversionClicked(User user);
+}
